f27eed47166f1e17168d26c0f66d1c0d8a0e3911
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2026-09-25 19:26:02 -0300

    Delete backend/src/main directory

--- backend/src/main/java/com/cti/provedor/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cti.provedor.repository;
-
-import com.cti.provedor.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
